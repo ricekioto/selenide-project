7e167f3a91625e822b65b8d8373788f8c40b3822
mmkit <ataichukuev3@gmail.com>
2025-09-27 10:49:10 +0500

    unit

--- src/test/java/com/example/selenideproject/demoqa/pages/ElementsPage.java
+++ src/test/java/com/example/selenideproject/demoqa/pages/ElementsPage.java
@@ -1,5 +1,13 @@
 package com.example.selenideproject.demoqa.pages;
 
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideElement;
+
 public class ElementsPage {
+    private final SelenideElement textBoxCard = Selenide.$x("//span[text()='Text Box']");
+
+    public void textBoxCardClick() {
+        textBoxCard.click();
+    }
 
 }
