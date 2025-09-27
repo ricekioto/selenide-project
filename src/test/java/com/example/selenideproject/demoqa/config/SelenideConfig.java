4ff279aaaf7b1f16b4a3ea406810c7605859c164
mmkit <ataichukuev3@gmail.com>
2025-09-27 10:41:53 +0500

    unit

--- src/test/java/com/example/selenideproject/demoqa/config/SelenideConfig.java
+++ src/test/java/com/example/selenideproject/demoqa/config/SelenideConfig.java
@@ -1,4 +1,4 @@
-package com.example.selenideproject.config;
+package com.example.selenideproject.demoqa.config;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.AfterAll;
@@ -14,7 +14,7 @@ public interface SelenideConfig {
         Configuration.browserSize = "chrome";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.timeout = 1000;
-        Configuration.pageLoadTimeout = 150000;
+        Configuration.pageLoadTimeout = 150000;;
     }
     @AfterAll
     static void teardown() {
