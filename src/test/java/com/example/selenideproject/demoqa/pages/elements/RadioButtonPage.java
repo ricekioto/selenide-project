15102ca84a580b901629a73da6caffe63e6f9764
mmkit <ataichukuev3@gmail.com>
2025-10-01 10:28:29 +0500

    unit

---
+++ src/test/java/com/example/selenideproject/demoqa/pages/elements/RadioButtonPage.java
@@ -0,0 +1,32 @@
+package com.example.selenideproject.demoqa.pages.elements;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class RadioButtonPage {
+    SelenideElement yesRadio = $x("//label[@for='yesRadio']");
+    SelenideElement impressiveRadio = $x("//label[@for='impressiveRadio']");
+    SelenideElement noRadio = $x("//label[@for='noRadio']");
+    SelenideElement result = $x("//p[@class='mt-3']");
+
+    public void clickYesRadio() {
+        yesRadio.click();
+    }
+
+    public void clickImpressiveRadio() {
+        impressiveRadio.click();
+    }
+
+    public void clickNoRadio() {
+        noRadio.click();
+    }
+
+    public void verifyResult(String text) {
+        result.shouldBe(Condition.text(text));
+    }
+}
