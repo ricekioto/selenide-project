a73e9d54049360391dbcb2c794d7dd0e65c67d44
mmkit <ataichukuev3@gmail.com>
2025-10-01 10:28:24 +0500

    unit

---
+++ src/test/java/com/example/selenideproject/demoqa/pages/elements/CheckBoxPage.java
@@ -0,0 +1,37 @@
+package com.example.selenideproject.demoqa.pages.elements;
+
+import com.codeborne.selenide.SelenideElement;
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$x;
+
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class CheckBoxPage {
+    SelenideElement angularSelector = $x("//span[contains(text(),'Angular')]");
+    SelenideElement generalSelector = $x("//span[contains(text(),'General')]");
+    SelenideElement notesSelector = $x("//span[contains(text(),'Notes')]");
+    SelenideElement result =  $x("//div[@id='result']");
+    SelenideElement buttonExpendAll = $x("//button[@title='Expand all']");
+
+    public void clickAngularSelector() {
+        angularSelector.scrollIntoView(true).click();
+    }
+
+    public void clickGeneralSelector() {
+        generalSelector.scrollIntoView(true).click();
+    }
+
+    public void clickNotesSelector() {
+        notesSelector.scrollIntoView(true).click();
+    }
+
+    public void clickbuttonExpendAll() {
+        buttonExpendAll.click();
+    }
+
+    public void verifyResult(String result) {
+        this.result.shouldHave(text(result));
+    }
+}
