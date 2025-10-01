4144cc2ca7deee5d5077cc10c5d2fbb5c3e5a9e3
mmkit <ataichukuev3@gmail.com>
2025-10-01 10:28:27 +0500

    unit

---
+++ src/test/java/com/example/selenideproject/demoqa/test/CheckBoxPageTest.java
@@ -0,0 +1,29 @@
+package com.example.selenideproject.demoqa.test;
+
+import com.example.selenideproject.demoqa.config.SelenideConfig;
+import com.example.selenideproject.demoqa.pages.elements.CheckBoxPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class CheckBoxPageTest implements SelenideConfig {
+    private static CheckBoxPage checkBoxPage;
+
+    @BeforeAll
+    public static void beforeAll() {
+        open("/checkbox");
+        checkBoxPage = new CheckBoxPage();
+    }
+
+    @Test
+    public void checkBoxPageTest() {
+        checkBoxPage.clickbuttonExpendAll();
+
+        checkBoxPage.clickAngularSelector();
+        checkBoxPage.clickGeneralSelector();
+        checkBoxPage.clickNotesSelector();
+
+        checkBoxPage.verifyResult("You have selected : notes angular general");
+    }
+}
