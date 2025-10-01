f4c9c9630fd130a8f83fa42733078e3bfde1ed5d
mmkit <ataichukuev3@gmail.com>
2025-10-01 10:28:45 +0500

    unit

---
+++ src/test/java/com/example/selenideproject/demoqa/test/ElementsPageTest.java
@@ -0,0 +1,30 @@
+package com.example.selenideproject.demoqa.test;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
+import com.example.selenideproject.demoqa.config.SelenideConfig;
+import com.example.selenideproject.demoqa.pages.elements.ElementsPage;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ElementsPageTest implements SelenideConfig {
+    private static ElementsPage elementsPage;
+
+    @BeforeAll
+    public static void beforeAll() {
+        Selenide.open("/elements");
+        elementsPage = new ElementsPage();
+    }
+
+    @Test
+    public void textBoxCardClickTest() {
+        elementsPage.textBoxCardClick();
+        Selenide.$("#app").shouldHave(Condition.text("Forms"));
+    }
+
+    @Test
+    public void checkBoxCardClickTest() {
+        elementsPage.checkBoxCardClick();
+        Selenide.$("#app").shouldHave(Condition.text("Check Box"));
+    }
+}
