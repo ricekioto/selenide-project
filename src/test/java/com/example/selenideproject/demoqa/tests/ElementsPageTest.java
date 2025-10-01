c31fb0946ecf9fe1902d772df3dae253bcf6191f
mmkit <ataichukuev3@gmail.com>
2025-10-01 10:28:21 +0500

    unit

--- src/test/java/com/example/selenideproject/demoqa/tests/ElementsPageTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.selenideproject.demoqa.tests;
-
-import com.example.selenideproject.demoqa.config.SelenideConfig;
-
-public class ElementsPageTest implements SelenideConfig {
-
-}
