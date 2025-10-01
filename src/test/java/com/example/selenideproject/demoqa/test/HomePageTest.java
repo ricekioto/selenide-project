1a655b75d93437654367db7841b1cc6f5d660a27
mmkit <ataichukuev3@gmail.com>
2025-10-01 10:28:15 +0500

    unit

--- src/test/java/com/example/selenideproject/demoqa/test/HomePageTest.java
+++ src/test/java/com/example/selenideproject/demoqa/test/HomePageTest.java
@@ -1,4 +1,4 @@
-package com.example.selenideproject.demoqa.tests;
+package com.example.selenideproject.demoqa.test;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
@@ -12,27 +12,25 @@ public class HomePageTest implements SelenideConfig {
     private static HomePage homePage;
 
     @BeforeAll
-    public static void start() {
-            homePage = new HomePage();
+    public static void beforeAll() {
+        Selenide.open("/");
+        homePage = new HomePage();
     }
 
     @Test
     public void elementsCardClickTest() {
-        Selenide.open("/");
         homePage.clickElementsCard();
         Selenide.$("#app").shouldHave(Condition.text("Elements"));
     }
 
     @Test
     public void formCardClickTest() {
-        Selenide.open("/");
         homePage.clickFormsCard();
         Selenide.$("#app").shouldHave(Condition.text("Forms"));
     }
 
     @Test
     public void BookStoreApplicationCardClickTest() {
-        Selenide.open("/");
         homePage.clickBookStoreCard();
         Selenide.$("#app").shouldHave(Condition.text("Book Store Application"));
     }
